e789d74704776e6c07f1f40a0562294f6f31343a
vapvin <tpehdj12@daum.net>
2020-03-02 12:50:32 +0900

    Inital Commit

---
+++ precache-manifest.a471f7d1f91ca0e7341cff7cef556f38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65ad5e1eb109f4d4d4b98a354cfbe9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61d60734e34cb1050fe9",
+    "url": "/static/js/2.3de4c70e.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/static/js/2.3de4c70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6f8cea5339d98fcaec",
+    "url": "/static/js/main.d60d392b.chunk.js"
+  },
+  {
+    "revision": "9f230a43ae86e7074185",
+    "url": "/static/js/runtime-main.660e2b0d.js"
+  }
+]);
